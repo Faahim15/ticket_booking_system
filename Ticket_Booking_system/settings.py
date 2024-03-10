de45683964050b10ed04696e1e3b6e49ad2ae9e3
Faahim <abdulkaderfahim6@gmail.com>
2024-03-10 20:57:31 +0600

    last commit

--- Ticket_Booking_system/settings.py
+++ Ticket_Booking_system/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://ticket-booking-system-gvch.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
